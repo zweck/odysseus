d34628ac25223454de4b0a4518ae63e426b43b7a
Phil Hauser <phil.hsr@gmail.com>
2015-10-06 13:24:38 +0100

    abstracted the utterance type test

--- src/lib/Narrative.js
+++ src/lib/Narrative.js
@@ -16,6 +16,15 @@ export default class Narrative {
 	}
 
 	/**
+	 * This method is the initialiser for the narrative class
+	 * @param  {Array<String>} narrative This method takes a scene and runs through it
+	 */
+	run(scene){
+		this._narrative = scene;
+		this.go();
+	}
+
+	/**
 	 * This is a scene progress count used in the `go()` method
 	 * @param  {integer} incAmount This is the amount to progress the narrative, default is 1
 	 */
@@ -25,14 +34,10 @@ export default class Narrative {
 	}
 
 	/**
-	 * This method is the initialiser for the narrative class
-	 * @param  {Array<String>} narrative This method takes a scene and runs through it
+	 * This method parses out the character at the start of the utterance and checks that they exist in the character class
+	 * @param  {string} narrative The utterance from the scene array
+	 * @return {string} character The character name from the utterance
 	 */
-	run(scene){
-		this._narrative = scene;
-		this.go();
-	}
-
 	getCharactersForNarrative(narrative){
 		var character = narrative.split(":")[0].trim();
 		if(this._characters.indexOf(character) < 0){
@@ -42,8 +47,19 @@ export default class Narrative {
 		}
 	}
 
+	/**
+	 * This method offsets the wait time by the amount of characters in the utterance
+	 * @param  {string} narrative The utterance from the scene array
+	 * @return {number}           The amount of characters * 100ms
+	 */
 	textLengthOffset(narrative){
 		return narrative.length * 100;
+	}
+
+
+	type(utterance){
+		var utteranceType = typeof utterance;
+		return utteranceType;
 	}
 
 	/**
@@ -58,58 +74,73 @@ export default class Narrative {
 		var narrative = this._narrative;
 
 		// initialise some vars
-		var character, utterance;
+		var utterance, utteranceType;
 		
 		// if we're still in a narrative
 		if( i < narrative.length ){
 
-			// and if the next element in the scene array is text
-			if (!parseInt(narrative[i]) && !narrative[i].is) {
+			utterance = narrative[i];
 
-				utterance = narrative[i];
+			// get the `type` of object in the scene array
+			utteranceType = this.type(utterance);
 
-				// get a the character from the front of the scene text string
-				character = this.getCharactersForNarrative(utterance);
-
-				// if the character in the narrative isn't in the characters setup
-				// assume that its the protaganist
-				if(!character){
-
-					switch(this._perspective) {
-						case 1:
-							character = "Me";
-						break;
-						case 3:
-							character = "You";
-						break;
-						default:
-							character = "Me";
-					}
-					
-				}else{
-
-					// remove the character from the text string
-					utterance = utterance.replace(character + ":", "");
-				}
-
-				// pass the character and the text to the narrative view
-				this._narrativeView.render({output: utterance, character: character});
-
-				// increment the progress
-				this.incrementProgress();
-
-				// repeat
-				this.go();
-			}else if (parseInt(narrative[i])){
-
-				// if the array element is an integer, pass the int and the array index to the wait method
-				this.wait(narrative[i], i);
-			}else if(narrative[i].is === "decision"){
-				this.decide(narrative[i]);
+			// decide which method to run on the scene utterance
+			switch(utteranceType) {
+				case "string":
+					// if the array element is a string pass it to the say method
+					this.say(utterance);
+				break;
+				case "number":
+					// if the array element is an integer, pass the int and the array index to the wait method
+					this.wait(utterance, i);
+				case "object":
+					// if the array element is an object pass it to the decide method
+					this.decide(utterance);
+				break;
+				default:
+					this.say(utterance);
 			}
 
 		}
 
+	}
+
+	say(utterance){
+
+		var character;
+		
+		// get a the character from the front of the scene text string
+		character = this.getCharactersForNarrative(utterance);
+
+		// if the character in the narrative isn't in the characters setup
+		// assume that its the protaganist
+		if(!character){
+
+			switch(this._perspective) {
+				case 1:
+					character = "Me";
+				break;
+				case 3:
+					character = "You";
+				break;
+				default:
+					character = "Me";
+			}
+			
+		}else{
+
+			// remove the character from the text string
+			utterance = utterance.replace(character + ":", "");
+		}
+
+		// pass the character and the text to the narrative view
+		this._narrativeView.render({output: utterance, character: character});
+
+		// increment the progress
+		this.incrementProgress();
+
+		// repeat
+		this.go();
 	}
 
 	decide(decision){
